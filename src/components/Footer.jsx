25f31dae22f6f9b0f05757481581715659ec76e5
CodeDroid99 <johnmwega2002@gmail.com>
2023-08-28 14:54:58 +0300

    add custom bg for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <h6 className="text-sm text-center w-full">
+    <h6 className="text-sm text-center w-full bg-blue-500 p-4">
       Created via 💙{" "}
       <a
         href="https://github.com/BilalGumus"
         target="_blank"
         alt="Bilal Gümüş's GitHub Account"
-        className="hover:underline"
+        className="hover:underline" rel="noreferrer"
       >
         Bilal Gümüş
       </a>
